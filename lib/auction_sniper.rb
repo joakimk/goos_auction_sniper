d5dd26a15d3e1be5d87ee6c4ff7a52a03e8c323d
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2012-05-22 22:10:16 +0200

    Adding in the AuctionMessageTranslator (like in fig 12.2).

--- lib/auction_sniper.rb
+++ lib/auction_sniper.rb
@@ -1,12 +1,19 @@
 require "chat"
 require "ui"
+require "auction_message_translator"
 
 class AuctionSniper
   def initialize(ui, item_id)
+    @ui = ui
+    translator = AuctionMessageTranslator.new(self)
     Chat.instance.listen("sniper", "auction-#{item_id}") do |message|
-      ui.status = "lost"
+      translator.process_message(Chat.instance, message)
     end
     Chat.instance.send_message("sniper", "auction-#{item_id}", "SOLVersion: 1.1; Command: JOIN;")
+  end
+
+  def auction_closed
+    @ui.status = "lost"
   end
 end
 
